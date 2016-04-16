c294515e623517b4554571f0c7b1e5a09621abb8
Michael Contento <michaelcontento@gmail.com>
2016-04-16 16:09:07 +0200

    implement variable map sections

--- src/js/states/Main.js
+++ src/js/states/Main.js
@@ -1,15 +1,33 @@
 import { State } from 'phaser';
 
+import { totalSections } from '../config';
+
 export default class extends State {
     create() {
-        const map = this.game.add.tilemap('game');
-        map.addTilesetImage('roguelikeSheet_transparent', 'roguelikeSheet_transparent');
+        const sectionList = this._generateSectionList(4);
+        this._loadSectionList(sectionList);
+    }
 
-        const block = map.createLayer('block-01');
-        block.fixedToCamera = false;
+    _generateSectionList(len = 1) {
+        const sections = ['start'];
+        while (sections.length <= len) {
+            const nbr = this.rnd.integerInRange(1, totalSections);
+            sections.unshift(`section-${nbr}`);
+        }
+        return sections;
+    }
 
-        const layer = map.createLayer('start');
-        layer.y = 303;
-        layer.fixedToCamera = false;
+    _loadSectionList(sectionList) {
+        let lastLayerPosY = 0;
+        for (const sectionName of sectionList) {
+            const map = this.game.add.tilemap(sectionName);
+            map.addTilesetImage('roguelikeSheet_transparent', 'roguelikeSheet_transparent');
+
+            const layer = map.createLayer(map.layer.name);
+            layer.fixedToCamera = false;
+            layer.y = lastLayerPosY;
+
+            lastLayerPosY += map.height * 16 - 1;
+        }
     }
 }
--- src/js/states/Splash.js
+++ src/js/states/Splash.js
@@ -1,9 +1,15 @@
 import { State, Tilemap } from 'phaser';
 
+import { totalSections } from '../config';
+
 export default class extends State {
     preload() {
-        this.load.tilemap('game', 'maps/game.json', null, Tilemap.TILED_JSON);
+        this.load.tilemap('start', 'maps/start.json', null, Tilemap.TILED_JSON);
         this.load.image('roguelikeSheet_transparent', 'images/roguelikeSheet_transparent.png');
+
+        for (let i = 1; i <= totalSections; i++) {
+            this.load.tilemap(`section-${i}`, `maps/section-${i}.json`, null, Tilemap.TILED_JSON);
+        }
     }
 
     create() {
