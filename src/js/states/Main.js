4f7cb76ac84b5c5826d41f74d18725495433e5ef
Michael Contento <michaelcontento@gmail.com>
2016-04-16 13:35:32 +0200

    add background color in Main state

--- src/js/states/Main.js
+++ src/js/states/Main.js
@@ -2,6 +2,7 @@ import { State } from 'phaser';
 
 export default class extends State {
     create() {
+        this.stage.backgroundColor = '#3171CA';
         this.game.add.text(0, 0, 'Hallo Welt');
     }
 }
